afb16f6c4c0690201103b6d25f935a23a5f287e0
Matt Watson <mattwatson@Matts-MacBook-Pro.local>
2015-03-03 14:08:57 +0000

    WIP MKDO Admin

--- index.php
+++ index.php
@@ -593,6 +593,28 @@ class MKDO_Admin extends MKDO_Class {
 
 	}
 
+	/**
+	 * Fired during plugin deactivation.
+	 *
+	 * This class defines all code necessary to run during the plugin's deactivation.
+	 *
+	 * @since      1.0.0
+	 */
+	class MKDO_Admin_Deactivator {
+
+		/**
+		 * Short Description. (use period)
+		 *
+		 * Long Description.
+		 *
+		 * @since    1.0.0
+		 */
+		public static function deactivate() {
+
+		}
+
+	}
+
 }
 
 /**
